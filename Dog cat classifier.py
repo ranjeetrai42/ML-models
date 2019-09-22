398a23411c37787de2927d5c89f41a7f8e5b50cd
ranjeetrai42 <32496534+ranjeetrai42@users.noreply.github.com>
2019-09-23 02:28:43 +0530

    Add files via upload

---
+++ Dog cat classifier.py
@@ -0,0 +1,65 @@
+#Data set downloaded from kaggle
+
+# Part 1 - Building the CNN
+
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+#convolution
+classifier.add(Convolution2D(32,3,3,input_shape=(64,64,3),activation='relu'))
+
+#pooling
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+#convolutional layer 2
+classifier.add(Convolution2D(32,3,3,activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+
+
+#flattening
+classifier.add(Flatten())
+
+#adding classic ann
+classifier.add(Dense(output_dim=128,activation='relu'))
+classifier.add(Dense(output_dim=1,activation='sigmoid'))
+
+classifier.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
+
+#fitting the cnn to the images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+        'dataset/training_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+        'dataset/test_set',
+        target_size=(64,64),
+        batch_size=32,
+        class_mode='binary')
+
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch=8000,
+        epochs=25,
+        validation_data=test_set,
+        validation_steps=800)
